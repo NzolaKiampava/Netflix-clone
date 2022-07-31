77a6dc1c484fcdfcf4ee3417ebfadb3ad9a49f3c
Nzola <nzolakiampava@gmail.com>
2022-07-31 00:17:56 -0300

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,23 @@
+//Typically we would store in {process.env.API_KEY}
+const API_KEY =  'c309782e8d9d9a62dd7b894b32c98182';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
+
+
+
+//https://api.themoviedb.org/3/trending/all/week?api_key=c309782e8d9d9a62dd7b894b32c98182&language=en-US
+//https://api.themoviedb.org/3/discover/tv?api_key=c309782e8d9d9a62dd7b894b32c98182&with_networks=213
+//https://api.themoviedb.org/3/discover/movie?api_key=c309782e8d9d9a62dd7b894b32c98182&with_genres=27
+//https://api.themoviedb.org/3/discover/movie?api_key=c309782e8d9d9a62dd7b894b32c98182&with_genres=10749
+//https://api.themoviedb.org/3/discover/movie?api_key=c309782e8d9d9a62dd7b894b32c98182&with_genres=99
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZ3voJ1NBgyIOTV9QUce9VPl986vQmVyQ",
+    authDomain: "netflix-clone-75965.firebaseapp.com",
+    projectId: "netflix-clone-75965",
+    storageBucket: "netflix-clone-75965.appspot.com",
+    messagingSenderId: "1006479738354",
+    appId: "1:1006479738354:web:b9bdeeaa3f7282bf66d76b"
+  };
